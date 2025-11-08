c0bee05316e6618a5cbb7b0135612e0a5154c0b6
Jason Gantenberg <jason.gantenberg@gmail.com>
2025-11-08 13:07:35 -0500

    Fix problems with SILA fits in simulated data

--- inst/03_sila-sim.r
+++ inst/03_sila-sim.r
@@ -15,27 +15,36 @@ simlist <- qs_read(file.path(OUTPUT_DIR, "simulated-datasets.qs2"))
 # Function: Subset the data to include individuals with multiple scans within a given
 #    simulated dataset. Input must be an element of `simulated-datasets.qs2`.
 fmt_sim <- function(simdata) {
-  lu_multiscan <- simdata[, .N, .(sim, subid)][N > 1]
+  # impose a centiloid cap similar to maximum centiloids found in ADNI
+  # SILA does not handle the occasional extremes produced by exponential simulations
+  tmp <- simdata[centiloids_measured <= 350]
+  lu_multiscan <- tmp[, .N, .(sim, subid)][N > 1]
   multiscan_subset <- simdata[lu_multiscan, on = .(sim, subid)]
   stopifnot(all(multiscan_subset[, N] > 1))
-  out <- multiscan_subset[, .(sim, subid, xvalue, val = centiloids_measured)]
-  setkeyv(out, c("sim", "subid", "xvalue"))
+  out <- multiscan_subset[, .(sim, subid, age, val = centiloids_measured)]
+  setkeyv(out, c("sim", "subid", "age"))
   out
 }
 
-#  Function
-fit_sila <- function(fmt_data) {
-  ## print(names(fmt_data))
-  ## print(class(fmt_data))
-  plan(multisession) # start parallel processing
+#  Function: Run SILA over simulated datasets
+fit_sila <- function(fmt_data, parallel = TRUE) {
+  if (parallel == FALSE) {
+    plan(sequential)
+  } else {
+    plan(multisession) # start parallel processing
+  }
   fits <- foreach(i = unique(fmt_data$sim),
-                  .options.future = list(packages = c("data.table"))) %dofuture% {
+                  .options.future = list(packages = c("data.table", "silaR"),
+                                         seed = TRUE)) %dofuture% {
     tmp <- fmt_data[sim == i]
     currfit <- list()
     currfit[["res"]] <- sila(tmp, dt = SILA_DT, val0 = SILA_VAL0, maxi = SILA_MAX_ITER)
-    currfit[["resfit"]] <- sila_esimate(tmp$res$tsila, df = tmp)
+    currfit[["resfit"]] <- sila_estimate(currfit$res$tsila, df = tmp)
+    currfit
   }
-  plan(sequential) # end parallel processing
+  if (parallel == TRUE) {
+    plan(sequential) # end parallel processing
+  }
   fits
 }
 
@@ -54,4 +63,16 @@ fmt_simlog_hetero <- fmt_sim(simlist$simlog_hetero)
 ## RUN SILA ##
 ##########################################################################################
 
-fit_sila(fmt_simexp_homo)
+fitlist <- list()
+
+fitlist[["simexp_homo"]]   <- fit_sila(fmt_simexp_homo)
+fitlist[["simexp_hetero"]] <- fit_sila(fmt_simexp_homo)
+fitlist[["simlog_homo"]]   <- fit_sila(fmt_simexp_homo)
+fitlist[["simlog_hetero"]] <- fit_sila(fmt_simexp_homo)
+
+
+##########################################################################################
+## WRITE SILA ##
+##########################################################################################
+
+qs_save(fitlist, file.path(OUTPUT_DIR, "simulated-sila-fits.qs2"))
