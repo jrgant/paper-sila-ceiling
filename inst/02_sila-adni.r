6b9ab8029c88f226677bb33e34fc5195b6030f29
Jason Gantenberg <jason.gantenberg@gmail.com>
2025-11-09 22:21:29 -0500

    Use lowercase variable names

--- inst/02_sila-adni.r
+++ inst/02_sila-adni.r
@@ -11,50 +11,52 @@ source(here::here("inst", "00_constants.r"))
 # ADNIMERGE
 adnimerge <- fread(file.path(ADNI_PATH, qp("ADNIMERGE")))
 adnimerge[, joindate := EXAMDATE]
-setkeyv(adnimerge, c("RID", "EXAMDATE"))
+names(adnimerge) <- tolower(names(adnimerge))
+setkeyv(adnimerge, c("rid", "examdate"))
 
 ## Extract baseline dataset
 adni_age_bl <- adnimerge[, .(
-  BL_EXAM_DATE = first(EXAMDATE),
-  AGE_bl = first(AGE),
-  DX_bl = first(DX_bl)
-), keyby = RID]
+  bl_exam_date = first(examdate),
+  age_bl = first(age),
+  dx_bl = first(dx_bl)
+), keyby = rid]
 
 # AMYLOID PET SCANs (UC BERKELEY)
 berk <- fread(file.path(ADNI_PATH, qp("UCBERKELEY_AMY_6MM")))
+names(berk) <- tolower(names(berk))
 
 # ANALYSIS DATASET
 
 ## Inner join, dropping missing centiloids and pulling in baseline information
-AOUT_NMISS_CENTILOIDS <- berk[, sum(is.na(CENTILOIDS))]
-berkadni <- berk[!is.na(CENTILOIDS), .(RID, SCANDATE, CENTILOIDS)
-                 ][adni_age_bl, on = .(RID), nomatch = NULL]
+AOUT_NMISS_CENTILOIDS <- berk[, sum(is.na(centiloids))]
+berkadni <- berk[!is.na(centiloids), .(rid, scandate, centiloids)
+                 ][adni_age_bl, on = .(rid), nomatch = NULL]
 
 ## Include most recent DX status at first scan date
-berkadni_first_scan <- berkadni[, .SD[1], keyby = .(RID, SCANDATE)][, .(RID, SCANDATE)]
-berkadni_first_scan[, joindate := SCANDATE]
+berkadni_first_scan <- berkadni[, .SD[1], keyby = .(rid, scandate)][, .(rid, scandate)]
+berkadni_first_scan[, joindate := scandate]
 berkadni_first_scan <- adnimerge[berkadni_first_scan,
-                                 on = .(RID, joindate), roll = TRUE
-                                 ][, .(RID, SCANDATE, EXAMDATE, DX)]
+                                 on = .(rid, joindate), roll = TRUE
+                                 ][, .(rid, scandate, examdate, dx)]
 
 ## Create and add relevant variables to analytic dataset
-berkadni[, yrs_since_bl := (SCANDATE - BL_EXAM_DATE) / 365.25]
-berkadni[, age := round(AGE_bl + yrs_since_bl, digits = 1)]
-berkadni[berkadni_first_scan, on = .(RID), j = `:=`(DX_fs = i.DX,
-                                                    DX_fs_date = i.EXAMDATE)]
+berkadni[, yrs_since_bl := (scandate - bl_exam_date) / 365.25]
+berkadni[, age := round(age_bl + yrs_since_bl, digits = 1)]
+berkadni[berkadni_first_scan, on = .(rid), j = `:=`(dx_fs = i.dx,
+                                                    dx_fs_date = i.examdate)]
 
-berkadni[, `:=`(DX_bl_grp = fcase(DX_bl == "AD", "AD",
-                                  DX_bl == "CN", "CN",
-                                  DX_bl %in% c("EMCI", "LMCI", "SMC"), "MCI/SMC",
+berkadni[, `:=`(dx_bl_grp = fcase(dx_bl == "AD", "AD",
+                                  dx_bl == "CN", "CN",
+                                  dx_bl %in% c("EMCI", "LMCI", "SMC"), "MCI/SMC",
                                   default = NA_character_),
-                DX_fs_grp = fcase(DX_fs == "AD", "AD",
-                                  DX_fs == "CN", "CN",
-                                  DX_fs %in% c("EMCI", "LMCI", "SMC"), "MCI/SMC",
+                dx_fs_grp = fcase(dx_fs == "AD", "AD",
+                                  dx_fs == "CN", "CN",
+                                  dx_fs %in% c("EMCI", "LMCI", "SMC"), "MCI/SMC",
                                   default = NA_character_))]
 
-berkadni[is.na(DX_fs_grp), DX_fs_grp := DX_bl_grp]
+berkadni[is.na(dx_fs_grp), dx_fs_grp := dx_bl_grp]
 
-berkadni[, .SD[1], keyby = RID][, .N, .(DX_bl_grp, DX_fs_grp)]
+berkadni[, .SD[1], keyby = rid][, .N, .(dx_bl_grp, dx_fs_grp)]
 
 
 ##########################################################################################
@@ -62,9 +64,9 @@ berkadni[, .SD[1], keyby = RID][, .N, .(DX_bl_grp, DX_fs_grp)]
 ##########################################################################################
 
 # IDs for subjects with multiple PET scans
-lu_subid_all_multiscan <- berkadni[, .N, keyby = RID][N > 1, RID]
-berksub <- berkadni[RID %in% lu_subid_all_multiscan]
-setnames(berksub, c("RID", "CENTILOIDS"), c("subid", "val"))
+lu_subid_all_multiscan <- berkadni[, .N, keyby = rid][N > 1, rid]
+berksub <- berkadni[rid %in% lu_subid_all_multiscan]
+setnames(berksub, c("rid", "centiloids"), c("subid", "val"))
 
 # NOTE: Fitting SILA will throw warnings due to perfect fits in some of the drawn
 #       samples. Traceable to lm() and LOESS fits.
