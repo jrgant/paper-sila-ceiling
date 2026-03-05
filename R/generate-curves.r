c174827056bab8ce7d742e84201861d473cf35e4
Jason Gantenberg <jason.gantenberg@gmail.com>
2026-03-05 10:36:28 -0500

    Add offset for logistic curves

--- R/generate-curves.r
+++ R/generate-curves.r
@@ -4,14 +4,17 @@
 #' @param L Function maximum
 #' @param k Rate parameter
 #' @param x0 Midpoint of the logistic curve
+#' @param offset For the exponential function, the vertical shift. For the logistic
+#'   function, must be <= 0 (the default); setting it allows the logistic function
+#'   to elicit values < 0 while maintaining the desired maximum.
 #'
 #' @rdname generate-curves
 #' @export
-gen_logistic <- function(x, L, k = 1, x0 = 0) {
-  (L / (1 + exp(-k * (x - x0))))
+gen_logistic <- function(x, L, k = 1, x0 = 0, offset = 0) {
+  stopifnot(offset <= 0)
+  ((L + abs(offset)) / (1 + exp(-k * (x - x0)))) + offset
 }
 
-#' @param offset Vertical shift
 #' @rdname generate-curves
 #' @export
 gen_exponential <- function(x, k, x0, offset) {
