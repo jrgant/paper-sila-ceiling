b2870c351f202f6114789339713db49df7192fd8
Jason Gantenberg <jason.gantenberg@gmail.com>
2026-03-21 11:18:38 -0400

    Add linear function

--- R/generate-curves.r
+++ R/generate-curves.r
@@ -3,7 +3,8 @@
 #' @param x Point at which to evaluate function
 #' @param L Function maximum
 #' @param k Rate parameter
-#' @param x0 Midpoint of the logistic curve
+#' @param x0 Midpoint of the exponentential and logistic curve
+#' @param b Y-intercept for the linear model
 #' @param offset For the exponential function, the vertical shift. For the logistic
 #'   function, must be <= 0 (the default); setting it allows the logistic function
 #'   to elicit values < 0 while maintaining the desired maximum.
@@ -17,6 +18,12 @@ gen_logistic <- function(x, L, k = 1, x0 = 0, offset = 0) {
 
 #' @rdname generate-curves
 #' @export
-gen_exponential <- function(x, k, x0, offset) {
+gen_exponential <- function(x, k, x0, offset = 0) {
   offset + exp(k * (x - x0))
+}
+
+#' @rdname generate-curves
+#' @export
+gen_linear <- function(x, k, b) {
+  k * x + b
 }
